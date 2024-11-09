8eafdecf87b84a6f255ea4fff77f1c2f65d83f7e
Tegan Jain <teganjain527@gmail.com>
2024-11-09 11:51:59 +0530

    style admin page

--- frontend/src/components/Admin.tsx
+++ frontend/src/components/Admin.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import "../styles/Admin.css";
+import { Link } from 'react-router-dom';
 
 const Admin: React.FC = () => {
   const [password, setPassword] = useState('');
@@ -7,7 +9,7 @@ const Admin: React.FC = () => {
 
   const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // Prevent the default form submission behavior
-    if (password === 'yourpassword') { // Replace with your desired password
+    if (password === 'arghykadecimal') { // Replace with your desired password
       setAuthenticated(true);
       localStorage.setItem('authenticated', 'true');
       setError('');
@@ -22,27 +24,52 @@ const Admin: React.FC = () => {
   };
 
   return (
-    <div>
-      {authenticated || localStorage.getItem('authenticated') === 'true' ? (
-        <div>
-          <h2>Welcome, Admin!</h2>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <form onSubmit={handleLogin}>
-          <h2>Admin Login</h2>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter password"
-            required
-          />
-          <button type="submit">Login</button>
-          {error && <p style={{ color: 'red' }}>{error}</p>}
-        </form>
-      )}
-    </div>
+    <div className='container'>
+      {
+        authenticated || localStorage.getItem('authenticated') === 'true' ? (
+          <>
+            <div className="welcome-container">
+              <h2>Welcome, Admin!</h2>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+            <div className='header-container'>
+              <Link to="/data/3x3">
+                <button type='button'> 3x3 data entry </button>
+              </Link>
+              <Link to="/data/2x2">
+                <button type='button'> 2x2 data entry </button>
+              </Link>
+              <Link to="/data/4x4">
+                <button type='button'> 4x4 data entry </button>
+              </Link>
+              <Link to="/data/3x3oh">
+                <button type='button'> 3x3 OH data entry </button>
+              </Link>
+              <Link to="/data/pyra">
+                <button type='button'> pyra data entry </button>
+              </Link>
+            </div>
+          </>
+        ) : (
+          <div className="login-container">
+            <form onSubmit={handleLogin}>
+              <h2>Admin Login</h2>
+              <div className='input-container'>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Enter password"
+                  required
+                />
+                <button type="submit">Login</button>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+              </div>
+            </form>
+          </div>
+        )
+      }
+    </div >
   );
 };
 
