3e511e2e5ed9b8d2f3c3c5a7038bb96acec2c6c2
Saksham <sakshamkumar1415@gmail.com>
2024-12-14 17:35:14 +0530

    CRUD + backend auth done

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://0.0.0.0:3001/participants';
-export const UPDATE_URL = 'http://0.0.0.0:3001/update';
-export const DELETE_URL = 'http://0.0.0.0:3001/delete';
-export const ADMIN_AUTH_URL = 'http://0.0.0.0:3001/adminLogin';
-export const ADMIN_AUTH_API_URL = 'http://0.0.0.0:3001/api/adminLogin';
+export const BASE_URL = 'http://0.0.0.0:5000/participants';
+export const UPDATE_URL = 'http://0.0.0.0:5000/update';
+export const DELETE_URL = 'http://0.0.0.0:5000/delete';
+export const ADMIN_AUTH_URL = 'http://0.0.0.0:5000/adminLogin';
+export const ADMIN_AUTH_API_URL = 'http://0.0.0.0:5000/api/adminLogin';
